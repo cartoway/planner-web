b220b38c80ce4c5ca29a93dc86b0c25ba7e1e644
Gwénaël Rault <gwenael@cartoroute.com>
2023-12-20 14:10:28 +0100

    Edit export icons

--- app/controllers/concerns/planning_export.rb
+++ app/controllers/concerns/planning_export.rb
@@ -26,18 +26,18 @@ module PlanningExport
           locals: options.slice(:planning)
         )
       end
-      store_img_path = 'marker-home.png'
+      store_img_path = 'marker-store.png'
       zio.put_next_entry(store_img_path)
       zio.print IO.read('public/' + store_img_path)
       if options[:with_home_markers]
         (COLORS_TABLE + ['#000000']).each { |color|
-          img_path = 'marker-home-' + color[1..-1] + '.png'
+          img_path = 'marker-store-' + color[1..-1] + '.png'
           zio.put_next_entry(img_path)
           zio.print IO.read('public/' + img_path)
         }
       end
       (COLORS_TABLE + ['#707070']).each { |color|
-        img_path = 'point-' + color[1..-1] + '.png'
+        img_path = 'marker-' + color[1..-1] + '.png'
         zio.put_next_entry(img_path)
         zio.print IO.read('public/' + img_path)
       }
