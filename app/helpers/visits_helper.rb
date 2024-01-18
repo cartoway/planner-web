17b48440d3b74d8c06cc0d78124b93d866e93017
Gwénaël Rault <gwenael@cartoroute.com>
2024-01-18 12:14:15 +0100

    Add force_position to visit form

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -22,4 +22,17 @@ module VisitsHelper
   def visit_quantities(visit, vehicle, options = {})
     VisitQuantities.normalize(visit, vehicle, options)
   end
+
+  def visit_force_position_options_for_select
+    [
+      [t('activerecord.attributes.visits.force_position.neutral'), :neutral],
+      [t('activerecord.attributes.visits.force_position.always_first'), :always_first],
+      [t('activerecord.attributes.visits.force_position.always_last'), :always_last],
+      [t('activerecord.attributes.visits.force_position.never_first'), :never_first]
+    ]
+  end
+
+  def visit_force_position_options_for_select_selected(visit)
+    [t("activerecord.attributes.visits.force_position.#{visit.force_position}"), visit.force_position]
+  end
 end
