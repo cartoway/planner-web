5a6fbb1182bb5889e478d05e3d3f1d29e37746cc
Gwénaël Rault <gwenael@cartoroute.com>
2024-02-23 10:36:25 +0100

    DelayedJob avoid constent classes reload && reduce sleepiness

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
+Delayed::Worker.sleep_delay = 1
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
 Delayed::Worker.class_eval do
@@ -10,3 +11,23 @@ Delayed::Worker.class_eval do
     end
   end
 end
+
+module Delayed::WorkerClassReloadingPatch
+  # Override Delayed::Worker#reserve_job to optionally reload classes before running a job
+  def reserve_job(*)
+    job = super
+
+    if job && self.class.reload_app?
+      ActionDispatch::Reloader.cleanup!
+      ActionDispatch::Reloader.prepare!
+    end
+
+    job
+  end
+
+  # Override Delayed::Worker#reload! which is called from the job polling loop to not reload classes
+  def reload!
+    # no-op
+  end
+end
+Delayed::Worker.send(:prepend, Delayed::WorkerClassReloadingPatch)
