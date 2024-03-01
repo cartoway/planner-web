cde20908b6beb2571e033a28014856822522faea
Gwénaël Rault <gwenael@cartoway.com>
2024-03-01 10:04:45 +0100

    Edit color order

--- config/initializers/colors.rb
+++ config/initializers/colors.rb
@@ -1,2 +1,2 @@
-COLORS_TABLE = ['#1971c2', '#c92a2a', '#2f9e44', '#ff922b', '#ae3ec9', '#3bc9db', '#bf616a', '#3b4252', '#714B23', '#82c91e']
+COLORS_TABLE = ['#ff922b', '#82c91e', '#ae3ec9', '#3b4252', '#1971c2', '#c92a2a', '#2f9e44', '#3bc9db', '#bf616a', '#714B23']
 DEFAULT_COLOR = '#B5B5B5'
