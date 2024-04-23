6aacda95b8a2cdb01b54d510a3b0684d9338156d
Gwénaël Rault <gwenael@cartoway.com>
2024-04-23 10:14:42 +0200

    Fixes

--- config/initializers/bootstrap_form.rb
+++ config/initializers/bootstrap_form.rb
@@ -16,14 +16,14 @@ module BootstrapForm
       Haml::Engine.new(haml.strip_heredoc, format: :html5).render(self, locals)
     end
 
-    def submit(action: nil, message: nil, icon: nil, button: nil, title: nil)
+    def submit(action: nil, message: nil, icon: nil, button: nil, title: nil, disable_with: nil)
       action = action == 'new' ? 'create' : 'update'
       object = self.object.class.name.snakecase
 
-      render_haml <<-HAML, object: object, action: action, message: message, icon: icon
+      render_haml <<-HAML, object: object, action: action, message: message, icon: icon, disable_with: disable_with
         .form-group{ id: "#{object}_div_input" }
           .col-md-offset-2.col-md-6
-            %button{ name: 'button', type: 'submit', class: "#{button || 'btn btn-primary'}", title: "#{title}" }
+            %button{ name: 'button', type: 'submit', class: "#{button || 'btn btn-primary'}", title: "#{title}", data: { disable_with: "#{disable_with}"}}
               %i.fa{ class: "#{icon || 'fa-floppy-disk'}" }
               = message || I18n.t("helpers.submit.#{action}", model: I18n.t("activerecord.models.#{object}s.one"))
       HAML
