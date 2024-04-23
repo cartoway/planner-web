e7e62a6cfc3498ea64c33a97ab3d87146f67e17e
Gwénaël Rault <gwenael@cartoway.com>
2024-04-23 16:01:20 +0200

    Fix devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } # Needed by devise
   config.action_mailer.smtp_settings = {
     address: 'smtp.example.com',
     port: 587,
