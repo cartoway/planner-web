2a1176552c92178e13084e88cc542520ee571b60
Gwénaël Rault <gwenael@cartoroute.com>
2024-06-12 10:30:58 +0200

    Paginate destinations page (#128)

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -483,6 +483,11 @@ const destinations_form = function(params, api) {
       });
     }
   });
+
+  var table = $('#destination_box').find('table');
+  table.trigger('update');
+  table.trigger('applyWidgets');
+  table.trigger('appendCache');
 };
 
 
@@ -765,6 +770,10 @@ const destinations_index = function(params, api) {
           beforeSend: beforeSendWaiting,
           success: function() {
             $row.remove();
+            var table = $('#destination_box').find('table');
+            table.trigger('update');
+            table.trigger('applyWidgets');
+            table.trigger('appendCache');
             if (markers[id]) {
               var keys  = Object.keys(markers);
               var index = keys.indexOf(id);
@@ -953,6 +962,10 @@ const destinations_index = function(params, api) {
           $(".destinations").append(SMT['destinations/edit'](prepare_display_destination(data)));
           wire($('.destinations tr').last(), true)
           countInc();
+          var table = $('#destination_box').find('table');
+          table.trigger('update');
+          table.trigger('applyWidgets');
+          table.trigger('appendCache');
         },
         complete: completeWaiting,
         error: ajaxError
@@ -1144,7 +1157,7 @@ const destinations_index = function(params, api) {
       showProcessing: true,
       // Show order icon v and ^
       headerTemplate: '{content} {icon}',
-      widgets: ["uitheme", "filter", "columnSelector"],
+      widgets: ["uitheme", "filter", "columnSelector", "pager"],
       widgetOptions: {
         scroller_height: 220,
         // scroll tbody to top after sorting
@@ -1171,7 +1184,66 @@ const destinations_index = function(params, api) {
         columnSelector_container: $('#columnSelector'),
         columnSelector_name: 'data-selector-name',
         columnSelector_priority: 'data-priority',
-        columnSelector_layout: '<li role="presentation"><label><input type="checkbox">{name}</label></li>'
+        columnSelector_layout: '<li role="presentation"><label><input type="checkbox">{name}</label></li>',
+
+        // pager options
+
+        // css class names that are added
+        pager_css: {
+          container   : 'tablesorter-pager',    // class added to make included pager.css file work
+          errorRow    : 'tablesorter-errorRow', // error information row (don't include period at beginning); styled in theme file
+          disabled    : 'disabled'              // class added to arrows @ extremes (i.e. prev/first arrows "disabled" on first page)
+        },
+        // jQuery selectors
+        pager_selectors: {
+          container   : '.pager',       // target the pager markup (wrapper)
+          first       : '.first',       // go to first page arrow
+          prev        : '.prev',        // previous page arrow
+          next        : '.next',        // next page arrow
+          last        : '.last',        // go to last page arrow
+          gotoPage    : '.gotoPage',    // go to page selector - select dropdown that sets the current page
+          pageDisplay : '.pagedisplay', // location of where the "output" is displayed
+          pageSize    : '.pagesize'     // page size selector - select dropdown that sets the "size" option
+        },
+
+
+        // output default: '{page}/{totalPages}'
+        // possible variables: {size}, {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+        // also {page:input} & {startRow:input} will add a modifiable input in place of the value
+        pager_output: '{page:input} / {totalPages}',
+
+        // apply disabled classname to the pager arrows when the rows at either extreme is visible
+        pager_updateArrows: true,
+
+        // starting page of the pager (zero based index)
+        pager_startPage: 0,
+
+        // Reset pager to this page after filtering; set to desired page number
+        // (zero-based index), or false to not change page at filter start
+        pager_pageReset: 0,
+
+        // Number of visible rows
+        pager_size: 100,
+
+        // f true, child rows will be counted towards the pager set size
+        pager_countChildRows: false,
+
+        // Save pager page & size if the storage script is loaded (requires $.tablesorter.storage in jquery.tablesorter.widgets.js)
+        pager_savePages: true,
+
+        // Saves tablesorter paging to custom key if defined. Key parameter name
+        // used by the $.tablesorter.storage function. Useful if you have
+        // multiple tables defined
+        pager_storageKey: "tablesorter-pager",
+
+        // if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
+        // table row set to a height to compensate; default is false
+        pager_fixedHeight: false,
+
+        // remove rows from the table to speed up the sort of large tables.
+        // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+        pager_removeRows: false // removing rows in larger tables speeds up the sort
+
       }
     };
     $("#destination_box").find("table").tablesorter(tableOptions).bind('filterEnd', function() {
@@ -1184,6 +1256,13 @@ const destinations_index = function(params, api) {
           markersLayers.addLayer(marker);
         }
       });
+    }).bind('pagerChange pagerComplete pagerInitialized pageMoved', function(e, c) {
+      var p = c.pager,
+        msg = '"</span> event triggered, ' + (e.type === 'pagerChange' ? 'going to' : 'now on') +
+        ' page <span class="typ">' + (p.page + 1) + '/' + p.totalPages + '</span>';
+      $('#display')
+        .append('<li><span class="str">"' + e.type + msg + '</li>')
+        .find('li:first').remove();
     });
     $(".tablesorter-filter").addClass("form-control input-sm"); // filter_cssFilter not support setting multiple class at once
 
@@ -1209,6 +1288,10 @@ const destinations_index = function(params, api) {
               }
               countDec();
             });
+            var table = $('#destination_box').find('table');
+            table.trigger('update');
+            table.trigger('applyWidgets');
+            table.trigger('appendCache');
           },
           complete: completeWaiting,
           error: ajaxError
@@ -1218,47 +1301,7 @@ const destinations_index = function(params, api) {
   };
 
   const checkForDisplayDestinations = function(data) {
-    var isAccuracyDanger = data.destinations && $.grep(data.destinations, function(dest) {
-
-    return dest.geocoding_accuracy && dest.geocoding_accuracy < GlobalConfiguration.geocoderAccuracyWarning;
-    }).length > 0;
-
-    if (isAccuracyDanger) {
-
-      const displayDestinationsAfterModal = function() {
-        var cursorBody = $('body').css('cursor');
-        var cursorMap = $('#map').css('cursor');
-        $('body, #map').css({
-          cursor: 'progress'
-        });
-        setTimeout(function() {
-          displayDestinations(data);
-          $('body').css({
-            cursor: cursorBody
-          });
-          $('#map').css({
-            cursor: cursorMap
-          });
-        }, 200);
-      };
-
-      if (dialog_geocoding) dialog_geocoding.modal('hide');
-      $('#geocoding-danger-modal').modal({
-        keyboard: true,
-        show: true
-      });
-      $("#sort-geocoding").click(function() {
-        $('#geocoding-danger-modal').off('hidden.bs.modal', displayDestinationsAfterModal);
-        $('#geocoding-danger-modal').modal('hide');
-        sortList = [
-          [$('.tablesorter th').index($('th.geocoding')[0]), 0]
-        ];
-        displayDestinationsAfterModal();
-      });
-      $('#geocoding-danger-modal').on('hidden.bs.modal', displayDestinationsAfterModal);
-    } else {
-      displayDestinations(data);
-    }
+    displayDestinations(data);
   };
 
   var dialog_geocoding = bootstrap_dialog({
