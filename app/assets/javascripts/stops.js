571cad658ce5306fea690d4588d21fb1e89b232b
Gwénaël Rault <gwenael@cartoway.com>
2024-06-21 11:53:26 +0200

    toggle tracking

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -1,8 +1,6 @@
 'use strict';
 
-import { beforeSendWaiting, completeWaiting, ajaxError } from '../../assets/javascripts/ajax';
-
-const stops_edit = function(params) {
+export const stops_edit = function(params) {
   $('#radiobtn a').on('click', function(){
     var selected = $(this).data('title');
     var toggled = $(this).data('toggle');
@@ -20,50 +18,4 @@ const stops_edit = function(params) {
   function submitForm(current_context) {
     current_context.closest('form').submit();
   }
-
-
-  function getPosition() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(sendPosition, handleError);
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-
-  function sendPosition(position) {
-    var coords = position.coords;
-    console.log(coords);
-    $.ajax({
-      type: 'PATCH',
-      url: '/routes/' + params.route_id +'/update_position',
-      data: JSON.stringify(coords),
-      contentType : 'application/json',
-      beforeSend: function(jqXHR, settings) {
-        beforeSendWaiting();
-      },
-      complete: function(jqXHR, textStatus) {
-        completeWaiting();
-      }
-    });
-  }
-
-  function handleError(error) {
-    console.log(error);
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
-        break;
-      default:
-        alert("An error occurred: " + error.message);
-    }
-  }
-
-  getPosition();
-  setInterval(getPosition, 5 * 60 * 1000);
 };
-
-Paloma.controller('Stops', {
-  edit: function() {
-    stops_edit(this.params);
-  }
-});
