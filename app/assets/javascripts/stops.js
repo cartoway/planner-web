878daad61111e1f0bdbd46865aac823122608683
Gwénaël Rault <gwenael@cartoway.com>
2024-06-25 12:51:42 +0200

    Fix mobile js

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -4,14 +4,15 @@ export const stops_edit = function(params) {
   $('#radiobtn a').on('click', function(){
     var selected = $(this).data('title');
     var toggled = $(this).data('toggle');
-    $('#'+toggled).prop('value', selected);
-    $('#'+toggled+'_updated_at').prop('value', new Date().toUTCString());
+    $(this).closest('.input-group').find('#'+toggled).prop('value', selected);
+    $(this).closest('.input-group').find('#'+toggled+'_updated_at').prop('value', new Date().toUTCString());
+
 
     $('a[data-toggle="'+toggled+'"]').not('[data-title="'+selected+'"]').removeClass('active');
     $('a[data-toggle="'+toggled+'"][data-title="'+selected+'"]').addClass('active');
 
-    var match = $('#label-index').attr("class").match(new RegExp('label-[a-z]*'));
-    $('#label-index').removeClass(match.shift())
+    var match = $(this).closest('.panel').find('#label-index').attr("class").match(new RegExp('label-[a-z]*'));
+    $(this).closest('.panel').find('#label-index').removeClass(match.shift())
                      .addClass('label-'+selected);
     submitForm($(this));
   });
