18dac3bde1a9810c1f636a361c8325ca94e44ea1
Gwénaël Rault <gwenael@cartoway.com>
2024-06-28 14:39:34 +0200

    Fix planning controller

--- app/controllers/api_web/v01/plannings_controller.rb
+++ app/controllers/api_web/v01/plannings_controller.rb
@@ -1,11 +1,3 @@
-# Copyright © Mapotempo, 2016
-#
-# This file is part of Mapotempo.
-#
-# Mapotempo is free software. You can redistribute it and/or
-# modify since you respect the terms of the GNU Affero General
-# Public License as published by the Free Software Foundation,
-# either version 3 of the License, or (at your option) any later version.
 #
 # Mapotempo is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -15,58 +7,51 @@
 # along with Mapotempo. If not, see:
 # <http://www.gnu.org/licenses/agpl.html>
 #
-class ApiWeb::V01::ZoningsController < ApiWeb::V01::ApiWebController
+class ApiWeb::V01::PlanningsController < ApiWeb::V01::ApiWebController
   skip_before_action :verify_authenticity_token # because rails waits for a form token with POST
   load_and_authorize_resource
-  before_action :manage_zoning
-  around_action :includes_destinations
+  before_action :manage_planning
+  around_action :includes_sub_models, only: [:print]
 
   def edit
+    @spreadsheet_columns = []
+    @with_devices = true
     capabilities
   end
 
-  def update
-    respond_to do |format|
-      if @zoning.update_attributes(zoning_params) && @zoning.save
-        @zoning.errors.add(:base, 'test')
-        format.html { redirect_to api_web_v01_edit_zoning_path(@zoning), notice: t('activerecord.successful.messages.updated', model: @zoning.class.model_name.human) }
-      else
-        capabilities
-        format.html { render action: 'edit' }
-      end
-    end
+  def print
+    @params = params
+    respond_to(&:html)
   end
 
   def self.manage
-    Hash[[:edit, :organize].map{ |v| ["manage_#{v}".to_sym, true] }]
+    Hash[[:organize, :print].map{ |v| ["manage_#{v}".to_sym, true] }]
   end
 
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  # rights should be checked before thanks to CanCan::Ability
+  def manage_planning
+    @manage_planning = ApiWeb::V01::PlanningsController.manage
+    @callback_button = true
+  end
+
+  def includes_sub_models
+    if action_name.to_sym == :print
+      VehicleUsage.with_stores.scoping do
+        Route.includes_destinations.scoping do
+          yield
+        end
+      end
+    else
+      yield
+    end
+  end
 
   def capabilities
     @isochrone = []
     @isodistance = []
     @isoline_need_time = []
-  end
-
-  # Use callbacks to share common setup or constraints between actions.
-  # rights should be checked before thanks to CanCan::Ability
-  def manage_zoning
-    @manage_zoning = ApiWeb::V01::ZoningsController.manage
-  end
-
-  def includes_destinations
-    Route.includes_destinations.scoping do
-      yield
-    end
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def zoning_params
-    params[:zoning] ||= {id: params[:id]} # Require not empty if none zone
-    params[:zoning][:zones_attributes].each{ |zone|
-      zone[:speed_multiplier] = zone[:avoid_zone] ? 0 : 1
-    } if params[:zoning][:zones_attributes]
-    params.require(:zoning).permit(:name, zones_attributes: [:id, :name, :polygon, :_destroy, :vehicle_id, :speed_multiplier])
   end
 end
