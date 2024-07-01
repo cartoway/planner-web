90e4ffe6b0b12e7f36efb8ae94904e18b3f276a1
Gwénaël Rault <gwenael@cartoway.com>
2024-07-01 16:56:51 +0200

    Add user_agent test

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,8 +1,8 @@
 module MobileHelper
   def detect_agent
-    return :ios if request.user_agent.match('iPhone OS')
+    return :ios if controller.request.user_agent.match('iPhone OS')
 
-    return :mobile if request.user_agent.match('Mobile')
+    return :mobile if controller.request.user_agent.match('Mobile')
 
     :standard
   end
