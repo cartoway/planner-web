e04ea66268bbfa5c94dd8fee62843c9f244e9392
Gwénaël Rault <gwenael@cartoway.com>
2024-07-02 17:59:44 +0200

    Visual edits
    Add route quantities

--- app/assets/javascripts/stops.js
+++ app/assets/javascripts/stops.js
@@ -1,26 +1,42 @@
 'use strict';
 
 export const stops_edit = function(params) {
-  $('#radiobtn a').on('click', function(){
+  $('#radiobtn a, #quick-status').on('click', function() {
     var selected = $(this).data('title');
-    var toggled = $(this).data('toggle');
-    $(this).closest('.input-group').find('#'+toggled).prop('value', selected);
-    $(this).closest('.input-group').find('#'+toggled+'_updated_at').prop('value', new Date().toUTCString());
-
-
-    $('a[data-toggle="'+toggled+'"]').not('[data-title="'+selected+'"]').removeClass('active');
-    $('a[data-toggle="'+toggled+'"][data-title="'+selected+'"]').addClass('active');
-
-    var match = $(this).closest('.panel').find('#label-index').attr("class").match(new RegExp('label-[a-z]*'));
-    $(this).closest('.panel').find('#label-index').removeClass(match.shift())
-                     .addClass('label-'+selected);
-    var match = $(this).closest('.panel').find('.panel-heading').attr("class").match(new RegExp('panel-heading-[a-z]*'));
-    $(this).closest('.panel').find('.panel-heading').removeClass(match.shift())
-                     .addClass('panel-heading-'+selected);
-    submitForm($(this));
+    changeStatuses($(this), selected);
   });
 
+  function changeStatuses(element, selected) {
+    var panel = $(element).closest('.panel');
+    var toggled = $(element).data('toggle');
+    panel.find('.input-group').find('#'+toggled).prop('value', selected);
+    panel.find('.input-group').find('#'+toggled+'_updated_at').prop('value', new Date().toUTCString());
+
+
+    panel.find('#radiobtn a[data-toggle="'+toggled+'"]').not('[data-title="'+selected+'"]').removeClass('active');
+    panel.find('#radiobtn a[data-toggle="'+toggled+'"][data-title="'+selected+'"]').addClass('active');
+
+    var match = panel.find('#label-index').attr("class").match(new RegExp('label-([a-z]*)'));
+    panel.find('#label-index').removeClass(match.shift())
+                     .addClass('label-'+selected);
+    var match = panel.find('.panel-heading').attr("class").match(new RegExp('panel-heading-[a-z]*'));
+    panel.find('.panel-heading').removeClass(match.shift())
+                     .addClass('panel-heading-'+selected);
+
+    var next_status;
+    if(selected == 'intransit') {
+      next_status = 'delivered';
+      panel.find('#quick-status').removeClass('d-none');
+      panel.find('#quick-status').data('title', next_status);
+      panel.find('#quick-status-text').text(I18n.t("plannings.edit.stop_status." + next_status));
+    } else {
+      panel.find('#quick-status').addClass('d-none');
+    }
+    submitForm(panel);
+  }
+
   function submitForm(current_context) {
-    current_context.closest('form').submit();
+    console.log(current_context.find('form'));
+    current_context.find('form').submit();
   }
 };
