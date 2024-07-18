e3df2d02c047fef971f252571c6535a104a0f200
Gwénaël Rault <gwenael@cartoway.com>
2024-07-18 08:24:14 +0200

    Bump rails 6.1

--- config/initializers/bootstrap_form.rb
+++ config/initializers/bootstrap_form.rb
@@ -29,19 +29,19 @@ module BootstrapForm
     end
 
     def render_haml(haml, locals = {})
-      Haml::Engine.new(haml.strip_heredoc, format: :html5).render(self, locals)
+      Haml::Template.new { haml.strip_heredoc }.render(self, locals)
     end
 
     def submit(action: nil, message: nil, icon: nil, button: nil, title: nil, disable_with: nil)
       action = action == 'new' ? 'create' : 'update'
-      object = self.object.class.name.snakecase
+      object = self.object.class.name.underscore
 
       render_haml <<-HAML, object: object, action: action, message: message, icon: icon, disable_with: disable_with
         .row.form-group{ id: "#{object}_div_input" }
           .col-md-offset-2.col-md-6
             %button{ name: 'button', type: 'submit', class: "#{button || 'btn btn-primary'}", title: "#{title}", data: { disable_with: "#{disable_with}"}}
               %i.fa{ class: "#{icon || 'fa-floppy-disk'}" }
-              = message || I18n.t("helpers.submit.#{action}", model: I18n.t("activerecord.models.#{object}s.one"))
+              = message || I18n.t("helpers.submit.#{action}", model: I18n.t("activerecord.models.#{object.pluralize}.one"))
       HAML
     end
   end
