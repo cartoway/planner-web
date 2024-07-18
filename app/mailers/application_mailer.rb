c473085abd86e3e2d8f052208537910433fddbc7
Gwénaël Rault <gwenael@cartoway.com>
2024-07-18 16:43:59 +0200

    Fix deprecations & version change

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  add_template_helper(EmailHelper)
+  helper(EmailHelper)
 
   default from: Mapotempo::Application.config.default_from_mail
   layout 'mailer'
