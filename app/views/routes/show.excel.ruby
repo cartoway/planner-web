7fd3e472f96a194d59ee3df211bc557747d24406
Gwénaël Rault <gwenael@cartoway.com>
2024-07-19 09:48:06 +0200

    Ruby 3.0 syntax fixes and deprecations

--- app/views/routes/show.excel.ruby
+++ app/views/routes/show.excel.ruby
@@ -1,4 +1,4 @@
-CSV.generate({col_sep: ';', row_sep: "\r\n"}) { |csv|
+CSV.generate(**{col_sep: ';', row_sep: "\r\n"}) { |csv|
   csv << export_column_titles(@customer, @columns, @custom_columns)
   render partial: 'show', formats: [:csv], locals: {route: @route, csv: csv}
 }
--- test/api/v01/geocoder_test.rb
+++ test/api/v01/geocoder_test.rb
@@ -12,7 +12,7 @@ class V01::GeocoderTest < ActiveSupport::TestCase
 
   def api(part, param = {})
     part = part ? '/' + part.to_s : ''
-    "/api/0.1/geocoder/#{part}.json?api_key=testkey1&" + param.collect{ |k, v| "#{k}=" + URI.escape(v.to_s) }.join('&')
+    "/api/0.1/geocoder/#{part}.json?api_key=testkey1&" + param.collect{ |k, v| "#{k}=" + URI::DEFAULT_PARSER.escape(v.to_s) }.join('&')
   end
 
   test 'should geocode' do
