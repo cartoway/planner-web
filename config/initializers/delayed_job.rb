00cb5c833d62dfff317d6f5953d20384a8a41805
Gwénaël Rault <gwenael@cartoway.com>
2024-08-23 10:30:59 +0200

    Change job progress parsing

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,7 @@
+require 'delayed_job_progress_parser'
+
+Delayed::Job.include DelayedJobProgressParser
+
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.sleep_delay = 1
