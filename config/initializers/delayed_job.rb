08b419e3a70c62843452c7fa3cfc4b916184cb2c
Gwénaël Rault <gwenael@cartoway.com>
2024-08-26 09:34:25 +0200

    Revert "Change job progress parsing"
    This reverts commit 255c165f7df43a1fd576c1d68bca46f0f45838e3.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,3 @@
-require 'delayed_job_progress_parser'
-
-Delayed::Job.include DelayedJobProgressParser
-
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.sleep_delay = 1
