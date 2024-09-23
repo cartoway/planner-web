2cdfbc9b5fc0c27b51c70834d7bba97ea20a40a0
Gwénaël Rault <gwenael@cartoway.com>
2024-09-23 12:50:57 +0200

    Use sentry-rails instead of raven

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+if ENV['SENTRY_DSN']
+  Sentry.init { |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  }
+elsif ENV['APP_ENV'] == 'production'
+  puts 'WARNING: Sentry DSN should be defined for production'
+end
