ed078fa251f110193c5abc3a4df6d736a67f4d51
Gwénaël Rault <gwenael@cartoway.com>
2024-10-24 08:49:08 +0200

    Simplify zone polygons migration

---
+++ db/migrate/20241024064440_simplify_zone_polygons.rb
@@ -0,0 +1,10 @@
+class SimplifyZonePolygons < ActiveRecord::Migration[6.1]
+  def up
+    Zone.find_each{ |zone|
+      next if zone.polygon.nil? || JSON.parse(zone.polygon)['geometry'].nil?
+
+      zone.validate
+      zone.save!
+    }
+  end
+end
