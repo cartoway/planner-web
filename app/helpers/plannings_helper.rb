cb03a9724621ffd877c5daae9c1770d71e15b36c
Gwénaël Rault <gwenael@cartoway.com>
2024-11-07 16:57:07 +0100

    Planning  Use erb/haml instead of mustache template

--- app/helpers/plannings_helper.rb
+++ app/helpers/plannings_helper.rb
@@ -49,6 +49,18 @@ module PlanningsHelper
     end
   end
 
+  def planning_routes_move_array(routes)
+    routes.map{ |route|
+      {
+        route_id: route.id,
+        vehicle_usage_id: route.vehicle_usage_id,
+        vehicle_id: route.vehicle_usage&.vehicle_id,
+        name: (route.ref || '') + (route.vehicle_usage&.vehicle&.name || ''),
+        color: route.color || route.vehicle_usage&.vehicle&.color,
+      }.delete_if{ |_k, v| v.nil? }
+    }
+  end
+
   def self.route_quantities(planning, vehicle_usage)
     quantities = []
     planning.routes.find{ |route|
