b785895ddd5833a8a753a59f0d536740f640aec6
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-01-09 17:56:11 +0100

    Monkey patch RailsPerformance to log in JSON format

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -3,3 +3,27 @@ RailsPerformance.setup do |config|
   config.duration = 4.hours
   config.enabled = !Rails.env.test?
 end
+
+# Monkey patch, to log in JSON format
+if ENV['LOG_FORMAT'] == 'json'
+  module RailsPerformance
+    module Extensions
+      class ResourceMonitor
+        def store_data(data)
+          ::Rails.logger.info("RailsPerformance", server: server_id, context: context, role: role, data: data)
+
+          now = RailsPerformance::Utils.time
+          now = now.change(sec: 0, usec: 0)
+          RailsPerformance::Models::ResourceRecord.new(
+            server: server_id,
+            context: context,
+            role: role,
+            datetime: now.strftime(RailsPerformance::FORMAT),
+            datetimei: now.to_i,
+            json: data
+          ).save
+        end
+      end
+    end
+  end
+end
