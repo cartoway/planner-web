e02862d62f4ded0a4c49cb7724d372309d0dc5e3
mahdi lamriben <mehdilmbn@gmail.com>
2025-01-09 02:12:46 +0100

    change way to name in planning summary -> route.ref - vehicle.name

--- app/helpers/plannings_helper.rb
+++ app/helpers/plannings_helper.rb
@@ -58,7 +58,7 @@ module PlanningsHelper
           route_id: route.id,
           vehicle_usage_id: route.vehicle_usage_id,
           vehicle_id: route.vehicle_usage&.vehicle_id,
-          name: (route.ref || '') + (route.vehicle_usage&.vehicle&.name || ''),
+          name: [route.ref || '', route.vehicle_usage&.vehicle&.name || ''].compact_blank!.join(' - '),
           color: route.color || route.vehicle_usage&.vehicle&.color,
           hidden: route.hidden,
           locked: route.locked
