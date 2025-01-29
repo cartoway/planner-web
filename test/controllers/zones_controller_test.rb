79812a3d553f14bbcfe8d9bf106f10796720904c
Gwénaël Rault <gwenael@cartoway.com>
2025-01-29 12:50:14 +0100

    Costs - import & test & structure

--- test/controllers/zones_controller_test.rb
+++ test/controllers/zones_controller_test.rb
@@ -11,6 +11,6 @@ class ZonesControllerTest < ActionController::TestCase
   test 'should get show in excel' do
     get :show, params: { id: @zoning.zones.first, zoning_id: @zoning.id, format: :excel, locale: 'fr' }
     assert_response :success
-    assert_equal "b;destination_one;Rue des Lilas;MyString;33200;Bordeau;;49.1857;-0.3735;;;;MyString;MyString;\"\";zone_one;\"\";b;00:05:33;10:00;11:00;;;4;tag1;neutre\r".encode("iso-8859-1"), response.body.split("\n").find{ |l| l.start_with? 'b;destination_one' }
+    assert_equal "b;destination_one;Rue des Lilas;MyString;33200;Bordeau;;49.1857;-0.3735;;;;MyString;MyString;\"\";zone_one;\"\";b;00:05:33;10:00;11:00;;;4;;tag1;neutre\r".encode("iso-8859-1"), response.body.split("\n").find{ |l| l.start_with? 'b;destination_one' }
   end
 end
