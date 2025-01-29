a95dab95e23011b74855d37de0f6fe44479616b6
Gwénaël Rault <gwenael@cartoway.com>
2025-01-29 10:44:46 +0100

    Add cost params to api permits

--- app/api/v01/destinations.rb
+++ app/api/v01/destinations.rb
@@ -56,7 +56,7 @@ class V01::Destinations < Grape::API
 
       deliverable_unit_ids = current_customer.deliverable_units.map{ |du| du.id.to_s }
       nested_visit_custom_attributes = current_customer.custom_attributes.select(&:visit?).map(&:name)
-      p.permit(:ref, :name, :street, :detail, :postalcode, :city, :state, :country, :lat, :lng, :comment, :phone_number, :geocoding_accuracy, :geocoding_level, tag_ids: [], visits_attributes: [:id, :ref, :duration, :time_window_start_1, :time_window_end_1, :time_window_start_2, :time_window_end_2, :priority, :force_position, tag_ids: [], quantities: deliverable_unit_ids, quantities_operations: deliverable_unit_ids, custom_attributes: nested_visit_custom_attributes])
+      p.permit(:ref, :name, :street, :detail, :postalcode, :city, :state, :country, :lat, :lng, :comment, :phone_number, :geocoding_accuracy, :geocoding_level, tag_ids: [], visits_attributes: [:id, :ref, :duration, :time_window_start_1, :time_window_end_1, :time_window_start_2, :time_window_end_2, :priority, :revenue, :force_position, tag_ids: [], quantities: deliverable_unit_ids, quantities_operations: deliverable_unit_ids, custom_attributes: nested_visit_custom_attributes])
     end
 
     def present_geojson_destinations(params)
