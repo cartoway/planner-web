cbb4fa77c341407de69834f4c9ae8c95a0808550
mahdi lamriben <mehdilmbn@gmail.com>
2025-02-17 00:23:48 +0100

    create method emission hash

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -95,6 +95,16 @@ class Vehicle < ApplicationRecord
     errors.add :capacities, :negative_value, **{value: e.object[:value]} if e.is_a? Exceptions::NegativeErrors
   end
 
+  def self.emissions_hash
+    {
+      "nothing" => [I18n.t('vehicles.emissions.nothing', n: 0), '0.0'],
+      "light_petrol" => [I18n.t('vehicles.emissions.light_petrol', n: self.localize_numeric_value(2.71)), '2.71'],
+      "light_diesel" => [I18n.t('vehicles.emissions.light_diesel', n: self.localize_numeric_value(3.07)), '3.07'],
+      "light_lgp" => [I18n.t('vehicles.emissions.light_lgp', n: self.localize_numeric_value(1.77)), '1.77'],
+      "ngv" => [I18n.t('vehicles.emissions.ngv', n: self.localize_numeric_value(2.13)), '2.13'],
+    }
+  end
+
   def self.emissions_table
     [
       [I18n.t('vehicles.emissions_nothing', n: 0), '0.0'],
