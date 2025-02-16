fb7d7af2d92340673eb25ad5134fa8dcb57ee03d
mahdi lamriben <mehdilmbn@gmail.com>
2025-02-17 00:24:43 +0100

    add default key for fuel default

--- config/application.rb
+++ config/application.rb
@@ -162,7 +162,7 @@ module Planner
 
     # Default values for vehicles
     config.vehicle_consumption_default = 7.7
-    config.vehicle_fuel_type_default = "VL - Gazole".freeze
+    config.vehicle_fuel_type_default = "light_lgp".freeze
   end
 end
 
