9e20d78f44f9907e996fac4b3447c233b7c466f0
Mahdi Lamriben <55115774+MadzMed@users.noreply.github.com>
2025-02-25 09:18:43 +0100

    create new geom from zoneGeomery and extract layers during creation (#282)
    * create new geom from zoneGeomery and extract layers during creation
    
    * update changelog
    
    * delete console log
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Gwénaël Rault <rgwenael@gmail.com>

--- app/assets/javascripts/zonings.js
+++ app/assets/javascripts/zonings.js
@@ -367,8 +367,9 @@ export const zonings_edit = function(params) {
       geoJsonLayer = geom;
       geom = geom.getLayers()[0];
     } else {
-      geoJsonLayer = (new zoneGeometry(JSON.parse(zone.polygon))).addOverlay(zone);
-      geoJsonLayer.addLayer(geom);
+      geom = (new zoneGeometry(JSON.parse(zone.polygon))).addOverlay(zone);
+      geoJsonLayer = geom;
+      geom = geom.getLayers()[0];
     }
 
     featureGroup.addLayer(geom);
