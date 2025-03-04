7859779d7565cc31e4246f634827b462238d6aa5
mahdi lamriben <mehdilmbn@gmail.com>
2025-03-04 14:40:41 +0100

    add default to jsonb data

--- db/migrate/20250217092158_add_export_info_to_users.rb
+++ db/migrate/20250217092158_add_export_info_to_users.rb
@@ -1,5 +1,5 @@
 class AddExportInfoToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :exportable_columns, :jsonb
+    add_column :users, :exportable_columns, :jsonb, default: {}
   end
 end
