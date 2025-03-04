cc479898a7cec9c04f8ea6da2dbebef988da9ace
Gwénaël Rault <gwenael@cartoway.com>
2025-03-04 15:31:25 +0100

    Remove unused sms_from_customer_name

--- app/api/v01/helper/shared_params.rb
+++ app/api/v01/helper/shared_params.rb
@@ -66,7 +66,6 @@ module SharedParams # rubocop:disable Metrics/ModuleLength
     optional :print_barcode, type: String, values: ::Customer::PRINT_BARCODE, documentation: { desc: 'Print the Reference as Barcode'}
     optional :sms_template, type: String
     optional :sms_concat, type: Boolean
-    optional :sms_from_customer_name, type: Boolean
 
     optional :enable_external_callback, type: Boolean, documentation: { desc: 'Enable external callback' }
     optional :external_callback_url, type: String, documentation: { desc: 'External callback URL' }
