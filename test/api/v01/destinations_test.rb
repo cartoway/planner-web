27c1a2fab2f989c175b9ea8882b72381ee75a405
Gwénaël Rault <gwenael@cartoway.com>
2025-04-08 17:54:55 +0200

    Add Destinations put test with quantity label

--- test/api/v01/destinations_test.rb
+++ test/api/v01/destinations_test.rb
@@ -307,6 +307,46 @@ class V01::DestinationsTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should create bulk from json with quantity label' do
+    deliverable = deliverable_units(:deliverable_unit_one_one)
+    assert_difference('Destination.count', 1) do
+      assert_difference('Visit.count', 1) do
+        put api(), nil, input: {
+          destinations: [{
+            name: 'Nouveau client',
+            street: nil,
+            postalcode: nil,
+            city: 'Tule',
+            state: 'Limousin',
+            lat: 43.5710885456786,
+            lng: 3.89636993408203,
+            detail: nil,
+            comment: nil,
+            phone_number: nil,
+            ref: 'z',
+            geocoding_accuracy: nil,
+            foo: 'bar',
+            visits: [{
+              ref: 'v1',
+              quantities: [{deliverable_unit_label: deliverable.label, quantity: 1}],
+              time_window_start_1: '08:00',
+              time_window_end_1: '12:00',
+              time_window_start_2: '14:00',
+              time_window_end_2: '18:00',
+              duration: nil
+            }]
+          }]
+        }.to_json, CONTENT_TYPE: 'application/json'
+        assert last_response.ok?, last_response.body
+        assert_equal 1, JSON.parse(last_response.body).size, 'Bad response size: ' + last_response.body.inspect
+
+        get api()
+        assert_equal deliverable.id, JSON.parse(last_response.body).find{ |destination| destination['name'] == 'Nouveau client' }['visits'][0]['quantities'][0]['deliverable_unit_id']
+        assert_equal 1, JSON.parse(last_response.body).find{ |destination| destination['name'] == 'Nouveau client' }['visits'][0]['quantities'][0]['quantity']
+      end
+    end
+  end
+
   test 'should create bulk from json with time exceeding one day' do
     assert_difference('Destination.count', 1) do
       assert_difference('Planning.count', 1) do
