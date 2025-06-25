98054a6dd799246f1b9ec1461340250f388f6df1
Gwénaël Rault <gwenael@cartoway.com>
2025-06-25 12:30:57 +0200

    fix quantities send to optim

--- lib/optim/optimizer_wrapper.rb
+++ lib/optim/optimizer_wrapper.rb
@@ -280,7 +280,7 @@ class OptimizerWrapper
         }.delete_if{ |_k, v| v.nil? || v.respond_to?(:empty?) && v.empty? },
         priority: stop.priority && (stop.priority.to_i - 4).abs,
         quantities: units.map{ |unit|
-          next if stop.visit.nil? || stop.visit.default_pickups.key?(unit.id) && stop.visit.default_deliveries.key?(unit.id)
+          next if stop.visit.nil? || (!stop.visit.default_pickups.key?(unit.id) && !stop.visit.default_deliveries.key?(unit.id))
 
           quantity = (stop.visit.default_deliveries[unit.id] || 0) - (stop.visit.default_pickups[unit.id] || 0)
           {
