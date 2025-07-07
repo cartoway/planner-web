68f125fcb0989b4956df3e127cb474aa8c37b011
Gwénaël Rault <gwenael@cartoway.com>
2025-07-07 09:02:09 +0200

    Put intermediate stops behind an option

--- app/api/v100/routes.rb
+++ app/api/v100/routes.rb
@@ -13,8 +13,8 @@ class V100::Routes < Grape::API
         desc 'Move stop(s) to route. Append in order at end if automatic_insert is false.',
           detail: 'Set a new A route (or vehicle) for a stop which was in a previous B route in the same planning.',
           nickname: 'moveStops',
-          success: V01::Status.success(:code_204),
-          failure: V01::Status.failures
+          success: V100::Status.success(:code_204),
+          failure: V100::Status.failures
         params do
           requires :id, type: String, desc: SharedParams::ID_DESC
           requires :stop_ids, type: Array[Integer], desc: 'Ids separated by comma. You can specify ref (not containing comma) instead of id, in this case you have to add "ref:" before each ref, e.g. ref:ref1,ref:ref2,ref:ref3.', documentation: {param_type: 'form'}, coerce_with: CoerceArrayString
@@ -33,25 +33,25 @@ class V100::Routes < Grape::API
                   current_customer.save!
                 end
               rescue VRPNoSolutionError
-                error! V01::Status.code_response(:code_304), 304
+                error! V100::Status.code_response(:code_304), 304
               end
               if planning.customer.job_optimizer
-                present planning.customer.job_optimizer, with: V01::Entities::Job
+                present planning.customer.job_optimizer, with: V100::Entities::Job
               else
                 status 204
               end
             end
           rescue Exceptions::JobInProgressError
             status 409
-            present planning.customer.job_optimizer, with: V01::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
+            present planning.customer.job_optimizer, with: V100::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
           end
         end
 
         desc 'Move visit(s) to route. Append in order at end if automatic_insert is false.',
           detail: 'Set a new A route (or vehicle) for a visit which was in a previous B route in the same planning. Automatic_insert parameter allows to compute index of the stops created for visits.',
           nickname: 'moveVisits',
-          success: V01::Status.success(:code_204),
-          failure: V01::Status.failures
+          success: V100::Status.success(:code_204),
+          failure: V100::Status.failures
         params do
           requires :id, type: String, desc: SharedParams::ID_DESC
           requires :visit_ids, types: [Array[String], Array[Integer]], desc: 'Ids separated by comma. You can specify ref (not containing comma) instead of id, in this case you have to add "ref:" before each ref, e.g. ref:ref1,ref:ref2,ref:ref3.', documentation: {param_type: 'form'}, coerce_with: CoerceArrayString
@@ -75,30 +75,32 @@ class V100::Routes < Grape::API
                 current_customer.save!
               end
               rescue VRPNoSolutionError
-                error! V01::Status.code_response(:code_304), 304
+                error! V100::Status.code_response(:code_304), 304
               end
               if planning.customer.job_optimizer
-                present planning.customer.job_optimizer, with: V01::Entities::Job
+                present planning.customer.job_optimizer, with: V100::Entities::Job
               else
                 status 204
               end
             end
           rescue Exceptions::JobInProgressError
             status 409
-            present planning.customer.job_optimizer, with: V01::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
+            present planning.customer.job_optimizer, with: V100::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
           end
         end
 
         desc 'Add intermediate store to route. Append in order at the end of the route',
           detail: 'Set a new StopStore to the route. index parameter allows to insert the stop at the provided index in the route.',
           nickname: 'addStopStore',
-          success: V01::Status.success(:code_204),
-          failure: V01::Status.failures
+          success: V100::Status.success(:code_204),
+          failure: V100::Status.failures
         params do
           requires :id, type: String, desc: SharedParams::ID_DESC
           requires :index, type: Integer
         end
         post ':route_id/stores/:id' do
+          error!(V100::Status.code_response(:code_401, after: I18n.t('errors.routes.enable_store_stops')), 401) if !current_customer.enable_store_stops
+
           Route.includes_destinations.scoping do
             planning = current_customer.plannings.where(ParseIdsRefs.read(params[:planning_id])).first!
             raise Exceptions::JobInProgressError if Job.on_planning(planning.customer.job_optimizer, planning.id)
@@ -113,7 +115,7 @@ class V100::Routes < Grape::API
             end
           rescue Exceptions::JobInProgressError
             status 409
-            present planning.customer.job_optimizer, with: V01::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
+            present planning.customer.job_optimizer, with: V100::Entities::Job, message: I18n.t('errors.planning.already_optimizing')
           end
         end
       end
