cce8f7ae57d6b494e7804052465eddc660d300e8
Gwénaël Rault <gwenael@cartoway.com>
2025-09-26 15:49:14 +0200

    Put enqueue with delay in a transaction

--- app/services/delayed_job_manager.rb
+++ app/services/delayed_job_manager.rb
@@ -23,27 +23,29 @@ class DelayedJobManager
     def enqueue_with_delay(job_class, *args, delay_seconds: 30)
       return unless Planner::Application.config.delayed_job_use
 
-      job = job_class.new(*args)
+      Delayed::Job.transaction do
+        job = job_class.new(*args)
 
-      job_signature = build_job_signature(job_class, *args)
+        job_signature = build_job_signature(job_class, *args)
 
-      existing_job = Delayed::Job.where(
-        "handler LIKE ? AND handler LIKE ?",
-        "%#{job_class.name}%",
-        "%#{job_signature}%"
-      ).first
+        existing_job = Delayed::Job.where(
+          "handler LIKE ? AND handler LIKE ?",
+          "%#{job_class.name}%",
+          "%#{job_signature}%"
+        ).first
 
-      if existing_job && existing_job.locked_at.nil?
-        # Only update if the job is not currently running or locked
-        begin
-          existing_job.update_column(:run_at, delay_seconds.seconds.from_now)
-        rescue PG::TRSerializationFailure, PG::TRDeadlockDetected, ActiveRecord::StatementInvalid
-          # Job is being modified by another process, skip update
-          Rails.logger.debug "Skipping job update due to concurrency: #{job_class.name}"
+        if existing_job && existing_job.locked_at.nil?
+          # Only update if the job is not currently running or locked
+          begin
+            existing_job.update_column(:run_at, delay_seconds.seconds.from_now)
+          rescue PG::TRSerializationFailure, PG::TRDeadlockDetected, ActiveRecord::StatementInvalid
+            # Job is being modified by another process, skip update
+            Rails.logger.debug "Skipping job update due to concurrency: #{job_class.name}"
+          end
+          existing_job
+        else
+          Delayed::Job.enqueue(job, run_at: delay_seconds.seconds.from_now)
         end
-        existing_job
-      else
-        Delayed::Job.enqueue(job, run_at: delay_seconds.seconds.from_now)
       end
     end
 
