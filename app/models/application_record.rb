4bee20c82aefd64cdb163046bf6f1350be7bf237
Gwénaël Rault <gwenael@cartoway.com>
2025-10-02 15:17:40 +0200

    Disable joinable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -31,7 +31,7 @@ class ApplicationRecord < ActiveRecord::Base
   def self.import_transaction(obj, **options)
     retries = 0
     begin
-      ActiveRecord::Base.transaction(requires_new: true) do
+      ActiveRecord::Base.transaction(joinable: false, requires_new: true) do
         self.import(obj, **options)
       end
     rescue ActiveRecord::SerializationFailure => e
