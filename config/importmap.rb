c724a8511bf0744152c25cc23ec7e6662066a98b
Gwénaël Rault <gwenael@cartoway.com>
2026-02-16 09:49:10 +0100

    Init v2/destinations index

---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Copyright © Cartoway
+#
+# Importmap configuration - Prepared for Rails 6+ migration
+# When upgrading to Rails 6, add: gem 'importmap-rails' and run: bin/rails importmap:install
+#
+# Example pins for destinations v2:
+# pin "application"
+# pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# pin_all_from "app/javascript/controllers", under: "controllers"
