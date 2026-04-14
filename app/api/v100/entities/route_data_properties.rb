6f77062759fd7a168feded764a51bf77c0455eb5
Gwénaël Rault <gwenael@cartoway.com>
2026-04-14 10:39:46 +0200

    Expose route_data alerts and counts for routes and plannings

--- app/api/v100/entities/route_data_properties.rb
+++ app/api/v100/entities/route_data_properties.rb
@@ -23,4 +23,24 @@ class V100::Entities::RouteDataProperties < Grape::Entity
   expose :id, documentation: { type: Integer }
   expose :hidden, documentation: { type: 'Boolean' }
   expose :color, documentation: { type: String, desc: 'Color code with #. For instance: #FF0000.' }
+
+  expose :size_active, documentation: { type: Integer }
+  expose :size_destinations, documentation: { type: Integer }
+  expose :size_store_reloads, documentation: { type: Integer }
+  expose :stops_size, documentation: { type: Integer }
+  expose :no_geolocalization, documentation: { type: 'Boolean' }
+  expose :no_path, documentation: { type: 'Boolean' }
+  expose :out_of_capacity, documentation: { type: 'Boolean' }
+  expose :out_of_drive_time, documentation: { type: 'Boolean' }
+  expose :out_of_force_position, documentation: { type: 'Boolean' }
+  expose :out_of_max_distance, documentation: { type: 'Boolean' }
+  expose :out_of_max_reload, documentation: { type: 'Boolean' }
+  expose :out_of_max_ride_distance, documentation: { type: 'Boolean' }
+  expose :out_of_max_ride_duration, documentation: { type: 'Boolean' }
+  expose :out_of_relation, documentation: { type: 'Boolean' }
+  expose :out_of_skill, documentation: { type: 'Boolean' }
+  expose :out_of_window, documentation: { type: 'Boolean' }
+  expose :out_of_work_time, documentation: { type: 'Boolean' }
+  expose :unmanageable_capacity, documentation: { type: 'Boolean' }
+  expose :max_loads, documentation: { type: Hash, desc: 'Per deliverable unit id (jsonb).' }
 end
