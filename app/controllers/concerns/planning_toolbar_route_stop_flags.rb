a58836ba1a483eec1cda6d8c108445d84f37e583
Gwénaël Rault <gwenael@cartoway.com>
2026-05-04 15:36:47 +0200

    Fix lint

---
+++ app/controllers/concerns/planning_toolbar_route_stop_flags.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+# Copyright © Cartoway, 2026
+#
+# This file is part of Cartoway Planner.
+#
+# Cartoway Planner is free software. You can redistribute it and/or
+# modify since you respect the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Cartoway Planner is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Cartoway Planner. If not, see:
+# <http://www.gnu.org/licenses/agpl.html>
+#
+
+# Maps route and stop toolbar segment permissions onto @manage_planning for the planning UI.
+module PlanningToolbarRouteStopFlags
+  extend ActiveSupport::Concern
+
+  private
+
+  def apply_route_toolbar_operation_flags!
+    return unless current_user
+
+    route_op_visible = method(:route_operation_visible?)
+    route_op_disabled = method(:route_operation_disabled?)
+
+    route_callback_visible =
+      route_op_visible.call('external_callback') &&
+      @planning&.customer&.enable_external_callback? &&
+      @planning&.customer&.external_callback_url.present?
+    @manage_planning[:manage_route_external_callback] = route_callback_visible
+    @manage_planning[:disable_route_external_callback] =
+      route_callback_visible && route_op_disabled.call('external_callback')
+
+    route_export_visible = route_op_visible.call('export')
+    @manage_planning[:disable_route_export] = route_op_disabled.call('export')
+    manage_export = @manage_planning[:manage_export]
+    manage_print = @manage_planning[:manage_print]
+    @manage_planning[:manage_route_export_menu] = route_export_visible && manage_export
+    @manage_planning[:manage_route_export_print_only] = route_export_visible && !manage_export && manage_print
+
+    @manage_planning[:manage_route_optimize] = route_op_visible.call('optimize')
+    @manage_planning[:disable_route_optimize] = route_op_disabled.call('optimize')
+
+    @manage_planning[:manage_route_stops] = route_op_visible.call('stops')
+    @manage_planning[:disable_route_stops] = route_op_disabled.call('stops')
+
+    @manage_planning[:manage_route_view] = route_op_visible.call('view')
+    @manage_planning[:disable_route_view] = route_op_disabled.call('view')
+
+    @manage_planning[:manage_route_lock] = route_op_visible.call('lock')
+    @manage_planning[:disable_route_lock] = route_op_disabled.call('lock')
+
+    @manage_planning[:manage_route_paint] = route_op_visible.call('paint')
+    @manage_planning[:disable_route_paint] = route_op_disabled.call('paint')
+
+    @manage_planning[:manage_route_departure] = route_op_visible.call('departure')
+    @manage_planning[:disable_route_departure] = route_op_disabled.call('departure')
+
+    store_form_visible = !current_user.respond_to?(:form_visible?) || current_user.form_visible?(:stores)
+    store_form_usable = !current_user.respond_to?(:form_update?) || current_user.form_update?(:stores)
+    @manage_planning[:manage_store] = store_form_visible
+    @manage_planning[:disable_store_form] = store_form_visible && !store_form_usable
+
+    route_vehicle_usage_operation_visible = route_op_visible.call('vehicle_usage')
+    form_ok = !current_user.respond_to?(:form_visible?) || current_user.form_visible?(:vehicle_usages)
+    @manage_planning[:manage_route_vehicle_usage] =
+      route_vehicle_usage_operation_visible && @manage_planning[:manage_vehicle] && form_ok
+    form_update_ok = !current_user.respond_to?(:form_update?) || current_user.form_update?(:vehicle_usages)
+    @manage_planning[:disable_route_vehicle_usage] = route_op_disabled.call('vehicle_usage') || !form_update_ok
+  end
+
+  def apply_stop_toolbar_operation_flags!
+    return unless current_user
+
+    stop_op_visible = method(:stop_operation_visible?)
+    stop_op_disabled = method(:stop_operation_disabled?)
+
+    @manage_planning[:manage_stop_active] = stop_op_visible.call('active_stop')
+    @manage_planning[:disable_stop_active] = stop_op_disabled.call('active_stop')
+
+    @manage_planning[:manage_stop_move] = stop_op_visible.call('move_stop')
+    @manage_planning[:disable_stop_move] = stop_op_disabled.call('move_stop')
+
+    @manage_planning[:manage_stop_lock] = stop_op_visible.call('lock_stop')
+    @manage_planning[:disable_stop_lock] = stop_op_disabled.call('lock_stop')
+  end
+end
