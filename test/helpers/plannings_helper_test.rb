253194eaa1ed3f15fdbff58a529f073752ed421d
Gwénaël Rault <gwenael@cartoway.com>
2026-05-04 13:58:06 +0200

    Fix popup vehicle select

--- test/helpers/plannings_helper_test.rb
+++ test/helpers/plannings_helper_test.rb
@@ -3,4 +3,13 @@
 require 'test_helper'
 
 class PlanningsHelperTest < ActionView::TestCase
+  include PlanningsHelper
+
+  test 'planning_summary includes planning_id and routes for sidebar / stop popover data' do
+    planning = plannings(:planning_one)
+    s = planning_summary(planning)
+    assert_equal planning.id, s[:planning_id]
+    assert s.key?(:routes)
+    assert s[:routes].is_a?(Array)
+  end
 end
