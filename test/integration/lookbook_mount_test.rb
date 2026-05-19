78b0ea145dd7ae9a9af154e6bffa1601ea384573
Gwénaël Rault <gwenael@cartoway.com>
2026-05-19 11:03:37 +0200

    Refine and introduce Visual regression CI

--- test/integration/lookbook_mount_test.rb
+++ test/integration/lookbook_mount_test.rb
@@ -9,4 +9,13 @@ class LookbookMountTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match(/lookbook/i, response.body)
   end
+
+  test 'planning sidebar bootstrap 5 preview renders' do
+    get '/lookbook/preview/design_system/planning_sidebar/default'
+
+    assert_response :success
+    assert_select 'body.cartoway-v2[data-controller="lookbook-v2"]', 1
+    assert_select '#edit-planning.sidebar', 1
+    assert_select '.lookbook-planning-sidebar-host', 1
+  end
 end
