aad9269338d00733c4e2cfd7d6798ee99ad15421
Gwénaël Rault <gwenael@cartoway.com>
2026-05-22 09:11:53 +0200

    Fix lint

--- test/models/concerns/planning_stops_preload_test.rb
+++ test/models/concerns/planning_stops_preload_test.rb
@@ -10,7 +10,7 @@ class PlanningStopsPreloadTest < ActiveSupport::TestCase
 
   test 'full mode when total stops are below both thresholds' do
     @customer.update!(stops_preload_limit: 1000)
-    set_planning_stops_size(500)
+    assign_planning_stops_size(500)
 
     assert_equal :full, PlanningStopsPreload.preload_mode(@planning.reload)
     assert PlanningStopsPreload.preload_stops?(@planning)
@@ -18,7 +18,7 @@ class PlanningStopsPreloadTest < ActiveSupport::TestCase
 
   test 'continuous mode between min and max thresholds' do
     @customer.update!(stops_preload_limit: 2000)
-    set_planning_stops_size(1500)
+    assign_planning_stops_size(1500)
 
     assert_equal :continuous, PlanningStopsPreload.preload_mode(@planning.reload)
     assert_not PlanningStopsPreload.preload_stops?(@planning)
@@ -26,14 +26,14 @@ class PlanningStopsPreloadTest < ActiveSupport::TestCase
 
   test 'continuous mode when limit is below fixed threshold' do
     @customer.update!(stops_preload_limit: 500)
-    set_planning_stops_size(750)
+    assign_planning_stops_size(750)
 
     assert_equal :continuous, PlanningStopsPreload.preload_mode(@planning.reload)
   end
 
   test 'manual mode when total stops reach the upper threshold' do
     @customer.update!(stops_preload_limit: 1000)
-    set_planning_stops_size(1000)
+    assign_planning_stops_size(1000)
 
     assert_equal :manual, PlanningStopsPreload.preload_mode(@planning.reload)
     assert_not PlanningStopsPreload.preload_stops?(@planning)
@@ -41,14 +41,14 @@ class PlanningStopsPreloadTest < ActiveSupport::TestCase
 
   test 'manual mode when total stops exceed customer limit above fixed threshold' do
     @customer.update!(stops_preload_limit: 2000)
-    set_planning_stops_size(2500)
+    assign_planning_stops_size(2500)
 
     assert_equal :manual, PlanningStopsPreload.preload_mode(@planning.reload)
   end
 
   private
 
-  def set_planning_stops_size(total_stops)
+  def assign_planning_stops_size(total_stops)
     routes = @planning.routes.select { |route| !route.hidden || !route.locked || route.vehicle_usage_id.nil? }.to_a
     per_route = total_stops / routes.size
     remainder = total_stops % routes.size
