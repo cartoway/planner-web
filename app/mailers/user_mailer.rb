f0ad0ef1fa87bc00b6ab99cfa8432b6cbd149009
Gwénaël Rault <gwenael@cartoway.com>
2026-06-05 08:50:39 +0200

    UserMailer - User might not belong to a customer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,15 +7,15 @@ class UserMailer < ApplicationMailer
   def password_message(user, locale)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
       @title = t('user_mailer.password.title')
-      @confirmation_link = password_user_url(user, token: user.confirmation_token, host: user.customer.reseller.url_protocol + '://' + user.customer.reseller.host)
+      @confirmation_link = password_user_url(user, token: user.confirmation_token, host: @reseller.url_protocol + '://' + @reseller.host)
       @subscription_duration = user.customer.end_subscription && (user.customer.end_subscription - Date.today).to_i > 1 ? (user.customer.end_subscription - Date.today).to_i : nil
-      @home_link = user.customer.reseller.url_protocol + '://' + user.customer.reseller.host
+      @home_link = @reseller.url_protocol + '://' + @reseller.host
       @template = 'password'
 
-      mail to: user.email, from: "#{@user.customer.reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.password.subject', name: @user.customer.reseller.name) do |format|
+      mail to: user.email, from: "#{@reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.password.subject', name: @reseller.name) do |format|
         format.html { render 'user_mailer/password', locals: { user: user } }
       end
     end
@@ -24,13 +24,13 @@ class UserMailer < ApplicationMailer
   def connection_message(user, locale)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
       @title = t('user_mailer.connection.title')
-      @home_link = user.customer.reseller.url_protocol + '://' + user.customer.reseller.host
+      @home_link = @reseller.url_protocol + '://' + @reseller.host
       @template = 'connection'
 
-      mail to: user.email, from: "#{@user.customer.reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.connection.subject', name: @user.customer.reseller.name) do |format|
+      mail to: user.email, from: "#{@reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.connection.subject', name: @reseller.name) do |format|
         format.html { render 'user_mailer/connection', locals: { user: user } }
       end
     end
@@ -39,15 +39,15 @@ class UserMailer < ApplicationMailer
   def accompanying_message(user, locale)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
       @template = 'accompanying'
       @parameters = links_parameters('accompanying_message', locale)
       @title = t('user_mailer.accompanying.title')
-      @home_link = user.customer.reseller.url_protocol + '://' + user.customer.reseller.host
+      @home_link = @reseller.url_protocol + '://' + @reseller.host
       @contact_url = contact_url(user, locale)
 
-      mail to: user.email, from: "#{@user.customer.reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.accompanying.title') do |format|
+      mail to: user.email, from: "#{@reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.accompanying.title') do |format|
         format.html { render 'user_mailer/accompanying', locals: { user: user } }
       end
     end
@@ -56,14 +56,14 @@ class UserMailer < ApplicationMailer
   def subscribe_message(user, locale)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
       @template = 'subscribe'
       @parameters = links_parameters('subscribe_message', locale)
       @title = t('user_mailer.subscribe.title')
-      @home_link = user.customer.reseller.url_protocol + '://' + user.customer.reseller.host
+      @home_link = @reseller.url_protocol + '://' + @reseller.host
 
-      mail to: user.email, from: "#{@user.customer.reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.subscribe.title') do |format|
+      mail to: user.email, from: "#{@reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t('user_mailer.subscribe.title') do |format|
         format.html { render 'user_mailer/subscribe', locals: { user: user } }
       end
     end
@@ -72,16 +72,16 @@ class UserMailer < ApplicationMailer
   def automation_dispatcher(user, locale, template = 'accompanying_team', with_links = false)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
-      @home_link = user.customer.reseller.url_protocol + '://' + user.customer.reseller.host
+      @home_link = @reseller.url_protocol + '://' + @reseller.host
       @template = template
       @help_url = user.customer.reseller.help_url.sub('{LG}', locale.to_s)
       @contact_url = contact_url(user, locale)
       @with_links = with_links
       @parameters = links_parameters(template, locale)
 
-      mail to: user.email, from: "#{@user.customer.reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t("user_mailer.#{template}.panels_header") do |format|
+      mail to: user.email, from: "#{@reseller.name} <#{Rails.application.config.default_from_mail}>", subject: t("user_mailer.#{template}.panels_header") do |format|
         format.html { render 'user_mailer/documentation_base', locals: { user: user } }
       end
     end
@@ -90,7 +90,7 @@ class UserMailer < ApplicationMailer
   def send_fleet_drivers(user, locale, drivers, current_admin = nil)
     I18n.with_locale(locale) do
       @user = user
-      @reseller = user.customer.reseller
+      @reseller = user.customer&.reseller || user.reseller
       @name, @application_name = names(user) # To deprecate
       @template = 'fleet_drivers'
       [user.email, current_admin && current_admin.email].compact.each do |to|
@@ -120,13 +120,13 @@ class UserMailer < ApplicationMailer
   end
 
   def contact_url(user, locale)
-    url = user.customer.reseller.contact_url
+    url = user.customer&.reseller&.contact_url || user.reseller.contact_url
     url.sub '{LG}', locale.to_s
   end
 
   def names(user)
-    name = user.customer.reseller.name # To deprecate
-    application_name = user.customer.reseller.application_name || name
+    name = user.customer&.reseller&.name || user.reseller&.name # To deprecate
+    application_name = user.customer&.reseller&.application_name || name
 
     [name, application_name]
   end
