8f701f6ad590a8a4544f4193e5c290342a44043b
Gwénaël Rault <gwenael@cartoway.com>
2026-06-23 12:10:02 +0200

    Init planning history

---
+++ app/controllers/planning_states_controller.rb
@@ -0,0 +1,161 @@
+# frozen_string_literal: true
+
+# Copyright © Cartoway, 2026
+#
+# This file is part of Cartoway Planner.
+#
+# Cartoway Planner is free software. You can redistribute it and/or
+# modify since you respect the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Cartoway Planner is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Cartoway Planner. If not, see:
+# <http://www.gnu.org/licenses/agpl.html>
+#
+
+require 'value_to_boolean'
+
+class PlanningStatesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_planning
+  before_action :set_planning_state, only: [:reapply, :destroy, :pin]
+
+  include PlanningsHelper
+  include ApplicationHelper
+  include PlanningStatesHelper
+  include PlanningToolbarPermissions
+  include PreferencesAuthorization
+
+  before_action :enforce_planning_states_operation!
+
+  def index
+    authorize! :read, @planning
+    @planning_states = @planning.planning_states.to_a
+
+    respond_to do |format|
+      format.json { render json: planning_states_json }
+    end
+  end
+
+  def reapply
+    authorize! :update, @planning
+
+    respond_to do |format|
+      if @planning.reapply_state!(@planning_state)
+        prepare_planning_show_json!
+        format.json { render template: 'plannings/show', formats: [:json] }
+      else
+        format.json { render json: { error: t('plannings.states.reapply_failed') }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    authorize! :update, @planning
+    @planning_state.destroy!
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  def pin
+    authorize! :update, @planning
+
+    pinned = ValueToBoolean.value_to_boolean(params[:pinned])
+    if pinned &&
+        PlanningState.pinned_count_for(@planning.id, @planning_state.category) >= PlanningState::MAX_PINNED_PER_GROUP &&
+        !@planning_state.pinned?
+      respond_to do |format|
+        format.json do
+          render json: { error: t('plannings.states.pin_limit_reached') }, status: :unprocessable_entity
+        end
+      end
+      return
+    end
+
+    @planning_state.update!(pinned: pinned)
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_planning
+    @planning = current_user.customer.plannings.where(id: params[:planning_id]).preload_route_details.first!
+  end
+
+  def set_planning_state
+    @planning_state = @planning.planning_states.find(params[:id])
+  end
+
+  def prepare_planning_show_json!
+    @planning = Planning.where(id: @planning.id).preload_routes_without_stops.first!
+    default_with_stops = PlanningStopsPreload.preload_mode(@planning) == :full
+    @with_stops = ValueToBoolean.value_to_boolean(params[:with_stops], default_with_stops)
+    @routes = if @with_stops
+      @planning.routes.available.includes_destinations_and_stores.includes_vehicle_usages
+    else
+      @planning.routes.available.includes_vehicle_usages
+    end
+    @with_devices = true
+  end
+
+  def enforce_planning_states_operation!
+    if %w[index].include?(action_name)
+      deny_unless_operation_visible!(:planning, 'planning_states')
+    else
+      deny_unless_operation_usable!(:planning, 'planning_states')
+    end
+  rescue PreferencesAuthorization::Forbidden
+    head :forbidden
+  end
+
+  def planning_states_json
+    reference_statistics = @planning.route_data_statistics
+    states_by_category = @planning_states.group_by(&:category)
+
+    PlanningState::CATEGORIES.filter_map do |category|
+      states = states_by_category[category]
+      next if states.blank?
+
+      pinned_count = states.count(&:pinned)
+
+      {
+        category: category,
+        category_label: t("plannings.states.categories.#{category}"),
+        max_pinned: PlanningState::MAX_PINNED_PER_GROUP,
+        pinned_count: pinned_count,
+        states: states.map { |state| planning_state_json(state, reference_statistics: reference_statistics) }
+      }
+    end
+  end
+
+  def planning_state_json(state, reference_statistics:)
+    statistics = state.statistics || {}
+    prefered_unit = current_user.prefered_unit
+
+    {
+      id: state.id,
+      category: state.category,
+      pinned: state.pinned,
+      captured_at: state.captured_at,
+      trigger: state.trigger,
+      trigger_label: t("plannings.states.triggers.#{state.trigger}", default: state.trigger),
+      captured_at_label: I18n.l(state.captured_at, format: :long),
+      statistics_html: planning_state_statistics_html(
+        statistics,
+        prefered_unit: prefered_unit,
+        reference_statistics: reference_statistics
+      ),
+      statistics: statistics
+    }
+  end
+end
---
+++ test/helpers/planning_states_helper_test.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+# Copyright © Cartoway, 2026
+#
+# This file is part of Cartoway Planner.
+#
+# Cartoway Planner is free software. You can redistribute it and/or
+# modify since you respect the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Cartoway Planner is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Cartoway Planner. If not, see:
+# <http://www.gnu.org/licenses/agpl.html>
+#
+
+require 'test_helper'
+
+class PlanningStatesHelperTest < ActionController::TestCase
+  tests PlanningStatesController
+
+  include PlanningStatesHelper
+
+  setup do
+    @reseller = resellers(:reseller_one)
+    request.host = @reseller.host
+    sign_in users(:user_one)
+    get :index, params: { planning_id: plannings(:planning_one).id, format: :json }
+    @helper_view = @controller.view_context
+  end
+
+  test 'planning_state_statistics_html renders route data blocks without speed or quantities' do
+    statistics = {
+      'routes_cost' => 42.5,
+      'routes_revenue' => 50.0,
+      'routes_drive_time' => 3600,
+      'routes_wait_time' => 600,
+      'routes_visits_duration' => 1800,
+      'routes_rests_duration' => 300,
+      'vehicles_used' => 2,
+      'vehicles' => 3,
+      'distance_total' => 12_500.0,
+      'routes_emission' => 1.25,
+      'duration_total' => 7200,
+      'work_duration_total' => 5400,
+      'stops_size' => 10,
+      'stops_size_active' => 8
+    }
+
+    html = @helper_view.planning_state_statistics_html(statistics, prefered_unit: 'km')
+
+    assert_includes html, 'route-info'
+    assert_includes html, 'route-data'
+    assert_not_includes html, 'fa-tachometer'
+    refute_match(/quantities/, html)
+  end
+
+  test 'planning_state_stat_diffs marks lower metrics as success and active stops inverted' do
+    state_stats = { 'distance_total' => 10_000.0, 'stops_size_active' => 8 }
+    reference_stats = { 'distance_total' => 12_000.0, 'stops_size_active' => 6 }
+
+    diffs = @helper_view.planning_state_stat_diffs(state_stats, reference_stats, prefered_unit: 'km')
+
+    assert_equal 'text-success', diffs['distance'][:css_class]
+    assert diffs['distance'][:formatted].start_with?('−')
+    assert_equal 'text-success', diffs['stops'][:css_class]
+    assert diffs['stops'][:formatted].start_with?('+')
+  end
+
+  test 'planning_state_stat_diffs marks higher metrics as danger except active stops' do
+    state_stats = { 'distance_total' => 15_000.0, 'stops_size_active' => 4 }
+    reference_stats = { 'distance_total' => 12_000.0, 'stops_size_active' => 7 }
+
+    diffs = @helper_view.planning_state_stat_diffs(state_stats, reference_stats, prefered_unit: 'km')
+
+    assert_equal 'text-danger', diffs['distance'][:css_class]
+    assert diffs['distance'][:formatted].start_with?('+')
+    assert_equal 'text-danger', diffs['stops'][:css_class]
+    assert diffs['stops'][:formatted].start_with?('−')
+  end
+
+  test 'planning_state_statistics_html renders diff markup when reference differs' do
+    statistics = {
+      'distance_total' => 10_000.0,
+      'stops_size' => 10,
+      'stops_size_active' => 8,
+      'routes_cost' => 42.5
+    }
+    reference_statistics = {
+      'distance_total' => 12_000.0,
+      'stops_size' => 10,
+      'stops_size_active' => 6,
+      'routes_cost' => 50.0
+    }
+
+    html = @helper_view.planning_state_statistics_html(
+      statistics,
+      prefered_unit: 'km',
+      reference_statistics: reference_statistics
+    )
+
+    assert_includes html, 'stat-diff'
+    assert_includes html, 'text-success'
+  end
+
+  test 'planning_state_stat_diffs marks higher revenue and balance as success' do
+    state_stats = { 'routes_revenue' => 60.0, 'routes_cost' => 30.0 }
+    reference_stats = { 'routes_revenue' => 50.0, 'routes_cost' => 40.0 }
+
+    diffs = @helper_view.planning_state_stat_diffs(state_stats, reference_stats, prefered_unit: 'km')
+
+    assert_equal 'text-success', diffs['total_revenue'][:css_class]
+    assert diffs['total_revenue'][:formatted].start_with?('+')
+    assert_equal 'text-success', diffs['balance'][:css_class]
+    assert diffs['balance'][:formatted].start_with?('+')
+  end
+
+  test 'planning_state_header_block_order excludes speed and quantities' do
+    order = @helper_view.planning_state_header_block_order
+
+    assert_not_includes order, 'speed'
+    assert_not_includes order, 'quantities'
+  end
+end
