34787c4956cf7bc3b89187ec6f776ec8192cd3c4
Gwénaël Rault <gwenael@cartoway.com>
2026-06-30 11:27:00 +0200

    Default route data values

--- test/models/route_data_test.rb
+++ test/models/route_data_test.rb
@@ -3,6 +3,21 @@
 require 'test_helper'
 
 class RouteDataTest < ActiveSupport::TestCase
+  test 'defaults computed route_data fields to safe import values' do
+    route_data = RouteData.new
+
+    assert_equal false, route_data.unmanageable_capacity
+    assert_equal false, route_data.out_of_capacity
+    assert_equal false, route_data.hidden
+    assert_equal 0, route_data.size_active
+    assert_equal 0, route_data.size_destinations
+    assert_equal 0, route_data.size_store_reloads
+    assert_equal 0, route_data.stops_size
+    assert_equal 0, route_data.size_active_destinations
+    assert_equal({}, route_data.max_loads)
+    assert_equal false, route_data.import_attributes['unmanageable_capacity']
+  end
+
   test 'duration sums visits rests wait and drive time' do
     route_data = RouteData.new(
       visits_duration: 120,
