6d536439a32ba5c2e3a6a9b114879cecf3604bf4
Gwénaël Rault <gwenael@cartoway.com>
2026-07-03 11:32:34 +0200

    Edit column width

--- test/helpers/destinations_helper_deliverable_unit_columns_test.rb
+++ test/helpers/destinations_helper_deliverable_unit_columns_test.rb
@@ -40,6 +40,16 @@ class DestinationsHelperDeliverableUnitColumnsTest < ActionView::TestCase
     assert_in_delta 3.0, data[:delivery], 1e-6
   end
 
+  test 'destinations_list_column_class maps column ids to css modifiers' do
+    assert_equal 'destinations-list-col destinations-list-col--name', destinations_list_column_class('name')
+    col_id = Preferences::Catalog::DestinationsList.deliverable_unit_column_id(@unit)
+    assert_equal 'destinations-list-col destinations-list-col--deliverable-unit', destinations_list_column_class(col_id)
+  end
+
+  test 'destinations_list_tags_tooltip joins tag labels' do
+    assert_equal 'tag1', destinations_list_tags_tooltip(destinations(:destination_two).tags.to_a)
+  end
+
   test 'destinations_list_visit_ref returns visit ref or dash' do
     visit = visits(:visit_one)
     assert_equal 'b', destinations_list_visit_ref(visit)
