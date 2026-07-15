556e69f8785cfe7516bb551da86dce337dcd8742
Gwénaël Rault <gwenael@cartoway.com>
2026-07-15 16:25:19 +0200

    Fix visual-regression

--- test/components/previews/design_system/tables_preview_test.rb
+++ test/components/previews/design_system/tables_preview_test.rb
@@ -3,12 +3,13 @@
 require 'test_helper'
 
 class DesignSystem::TablesPreviewTest < ActiveSupport::TestCase
-  test 'destinations_list render_args expose customer and list data to the template' do
+  test 'destinations_list render_args expose static sample data to the template' do
     args = DesignSystem::TablesPreview.render_args(:destinations_list)
 
     assert args[:assigns].present?
-    assert args[:assigns][:customer].present?
-    assert args[:assigns][:destinations].present?
+    assert_equal Lookbook::DestinationsListSample.customer, args[:assigns][:customer]
+    assert_equal 3, args[:assigns][:destinations].size
+    assert_equal 'Boulangerie Dupont', args[:assigns][:destinations].first.name
     assert args[:assigns][:destinations_list_columns].present?
     assert args[:assigns][:pagination].present?
     assert_includes args[:assigns][:destinations_list_columns], 'ref'
