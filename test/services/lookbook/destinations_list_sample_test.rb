60631c0a97f45283b88fb61bce68ea4f5dec3bd0
Gwénaël Rault <gwenael@cartoway.com>
2026-07-15 16:25:19 +0200

    Fix visual-regression

--- test/services/lookbook/destinations_list_sample_test.rb
+++ test/services/lookbook/destinations_list_sample_test.rb
@@ -17,4 +17,14 @@ class LookbookDestinationsListSampleTest < ActiveSupport::TestCase
 
     assert_includes Preferences::Catalog.destinations_list_allowed_column_ids(customer), 'visit_tags'
   end
+
+  test 'sample tags expose default icon and color for list badge helpers' do
+    destination = Lookbook::DestinationsListSample.destinations_for.first
+    urgent = destination.tags.find { |tag| tag.label == 'Urgent' }
+    retail = destination.tags.find { |tag| tag.label == 'Retail' }
+
+    assert_equal Planner::Application.config.destination_icon_default, urgent.default_icon
+    assert_equal '#c0392b', urgent.default_color
+    assert_equal 'fa-shopping-bag', retail.default_icon
+  end
 end
