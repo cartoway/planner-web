0f6436cefa0fe5f62cd63a31cd5b2dc741e0340d
Gwénaël Rault <gwenael@cartoway.com>
2026-09-11 10:40:47 +0200

    Stop info in visit form v2

--- app/assets/javascripts/plannings.js
+++ app/assets/javascripts/plannings.js
@@ -3792,6 +3792,8 @@ export const plannings_edit = function(params) {
       error: ajaxError,
       success: function() {
         updateSuccess(locals.summary, map, locals.routes);
+        var stopIdMatch = window.location.search.match(/[?&]stop_id=(\d+)/);
+        if (stopIdMatch) enlightenStop({ id: stopIdMatch[1] });
       },
       complete: completeAjaxMap
     });
